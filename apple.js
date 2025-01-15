cac00222ea0a63851e00a615f22fe4afca51194a
NurmuhammadAkhmedov <ahmedov13nurmuhammad@gmail.com>
2025-01-15 23:15:29 +0500

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingizni kiriting!!!");
+let name = prompt("Familyangizni kiriting!!!");
+let age = prompt("Yoshingizni kiriting!!!");
+let num = prompt("Qayerda yashaysizni kiriting!!!")
+let fan = prompt("Yoqtirgan faningizni kiriting!!!")
+console.log (ism)
+console.log(name)
+console.log(age)
+console.log(num)
+console.log(fan)
